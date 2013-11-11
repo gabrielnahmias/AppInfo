f2e2375df166828dbcd48fff66ef9be4ee39686f
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-11-11 01:52:54 -0600

    Rearranged properties, added GetVersion() method, and spruced up/improved code.

--- AppInfo.cs
+++ AppInfo.cs
@@ -1,68 +1,22 @@
-﻿using System;
+﻿/**
+ * AppInfo
+ *  @desc    Assists in gathering assembly information about the application.
+ *  @author  Gabriel Nahmias (gabriel@terrasoftlabs.com)
+ *  @ver     1.0
+ */
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Runtime.Versioning;
 
 /// <summary>
 /// Assists in gathering assembly information about the application.
 /// </summary>
 public class AppInfo
 {
-    static object[] attributes = Assembly.GetCallingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
-    public static Version Version {
-        get {
-            return Assembly.GetCallingAssembly().GetName().Version;
-        }
-    }
-
-    /// <summary>
-    /// Returns the title of the application.
-    /// </summary>
-    public static string Title
-    {
-        get
-        {
-            if (attributes.Length > 0)
-            {
-                AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
-                if (titleAttribute.Title.Length > 0) return titleAttribute.Title;
-            }
-            return Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
-        }
-    }
-
-    /// <summary>
-    /// Returns the product name of the application.
-    /// </summary>
-    public static string ProductName
-    {
-        get
-        {
-            return attributes.Length == 0 ? "" : ((AssemblyProductAttribute)attributes[0]).Product;
-        }
-    }
-
-    /// <summary>
-    /// Returns the description of the application.
-    /// </summary>
-    public static string Description
-    {
-        get
-        {
-            return attributes.Length == 0 ? "" : ((AssemblyDescriptionAttribute)attributes[0]).Description;
-        }
-    }
-
-    /// <summary>
-    /// Returns the copyright holder of the application.
-    /// </summary>
-    public static string CopyrightHolder
-    {
-        get
-        {
-            return attributes.Length == 0 ? "" : ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
-        }
-    }
+    static object[] attributes = Assembly.GetCallingAssembly().GetCustomAttributes(false);
 
     /// <summary>
     /// Returns the name of the company who made the application.
@@ -74,6 +28,10 @@ public class AppInfo
             return attributes.Length == 0 ? "" : ((AssemblyCompanyAttribute)attributes[0]).Company;
         }
     }
+
+    /// <summary>
+    /// Returns a DateTime object containing the moment when the application was last compiled.
+    /// </summary>
     public static DateTime CompileTime
     {
         get
@@ -106,4 +64,106 @@ public class AppInfo
         }
     }
 
+    /// <summary>
+    /// Returns the copyright for the application.
+    /// </summary>
+    public static string Copyright
+    {
+        get
+        {
+            return attributes.Length == 0 ? "" : ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+        }
+    }
+
+    /// <summary>
+    /// Returns the description of the application.
+    /// </summary>
+    public static string Description
+    {
+        get
+        {
+            return attributes.Length == 0 ? "" : ((AssemblyDescriptionAttribute)attributes[0]).Description;
+        }
+    }
+
+    /// <summary>
+    /// Returns the product name of the application.
+    /// </summary>
+    public static string ProductName
+    {
+        get
+        {
+            return attributes.Length == 0 ? "" : ((AssemblyProductAttribute)attributes[6]).Product;
+        }
+    }
+
+    /// <summary>
+    /// Returns the target framework for the application.
+    /// </summary>
+    public static string TargetFramework
+    {
+        get
+        {
+            return attributes.Length == 0 ? "" : ((TargetFrameworkAttribute)attributes[3]).FrameworkName;
+        }
+    }
+
+    /// <summary>
+    /// Returns the title of the application.
+    /// </summary>
+    public static string Title
+    {
+        get
+        {
+            // Got any attributes? If so, ...
+            if (attributes.Length > 0)
+            {
+                // ... get the title one and if it has an appropriate length, return it.
+                AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[4];
+                if (titleAttribute.Title.Length > 0) return titleAttribute.Title;
+            }
+            // Otherwise, get the application's filename and return it without its extension.
+            return Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+        }
+    }
+
+    /// <summary>
+    /// Returns the trademark for the application.
+    /// </summary>
+    public static string Trademark
+    {
+        get
+        {
+            return attributes.Length == 0 ? "" : ((AssemblyTrademarkAttribute)attributes[8]).Trademark;
+        }
+    }
+
+    /// <summary>
+    /// Returns the Version object for the application.
+    /// </summary>
+    public static Version Version
+    {
+        get
+        {
+            return Assembly.GetCallingAssembly().GetName().Version;
+        }
+    }
+
+    /* Methods */
+
+    /// <summary>
+    /// Given a format string (which recognizes characters 'M' [major], 'm' [minor], 'b' [build], and 'r' [revision]), this returns
+    /// a string formatted by this application's Version object's properties.
+    /// </summary>
+    /// <param name="fmt">The format string (any characters besides 'M' [major], 'm' [minor], 'b' [build], and 'r' [revision] will
+    /// appear as is.</param>
+    /// <returns>A string containing the specified parts of the application's version.</returns>
+    public static string GetVersion(string fmt = "M.m")
+    {
+        Version ver = Version;
+        return fmt.Replace("M", ver.Major.ToString())
+                  .Replace("m", ver.Minor.ToString())
+                  .Replace("b", ver.Build.ToString())
+                  .Replace("r", ver.Revision.ToString());
+    }
 }
